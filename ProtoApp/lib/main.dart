1c6705772459729f0cfcde4706e0005fc4fa0589
0025110320 <yoshiaki.hiroi@sony.com>
2020-10-27 12:10:23 +0900

    add Icon widget

--- ProtoApp/lib/main.dart
+++ ProtoApp/lib/main.dart
@@ -104,6 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            Icon(Icons.ac_unit_sharp),
           ],
         ),
       ),
